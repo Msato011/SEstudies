dd2b4c94bdc7918236ab2f0e7ccf3a65a27543ae
Msato011 <msato32168@gmail.com>
2025-04-22 19:09:21 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- src/App.vue -->
+<template>
+  <div class="app">
+    <h1>Hello Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.app {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
